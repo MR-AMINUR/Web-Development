de13cf6a57ec0b501bc164ee7466e5f247a40796
AMINUR <aminur01mallick@gmail.com>
2026-02-10 19:25:11 +0530

    properties of readFile Function.

--- frontend/script.js
+++ frontend/script.js
@@ -11,7 +11,12 @@ var3 = var1 + var2;
 
 const fs = require("fs");
 
-fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
+// fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
+//     if (err) throw err;
+//     console.log("The file has been saved!");
+// });
+
+fs.readFile("./message.txt", "utf8", (err, data) => {
     if (err) throw err;
-    console.log("The file has been saved!");
+    console.log(data);
 });
