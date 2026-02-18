e461350083b2c1101eec7c9e0824c3ed7150b5ca
AMINUR <aminur01mallick@gmail.com>
2026-02-18 19:57:23 +0530

    Practices

--- frontend/script.js
+++ frontend/script.js
@@ -21,9 +21,34 @@
 //     console.log(data);
 // });
 
-import superman from "superman";
+// import superman from "superman";
 
-const name = superman.random();
+// const name = superman.random();
 
-console.log(`I am ${name}!`);
+// console.log(`I am ${name}!`);
 
+/*
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered url into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/ 
+
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    {
+        message: "Type in your URL: ",
+        name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
