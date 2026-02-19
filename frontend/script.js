7ac7ed0fb033e3259e8d6fa420bfd3c405075748
AMINUR <aminur01mallick@gmail.com>
2026-02-19 23:36:33 +0530

    practices.

--- frontend/script.js
+++ frontend/script.js
@@ -34,6 +34,8 @@
 */ 
 
 import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
 
 inquirer
   .prompt([
@@ -44,6 +46,8 @@ inquirer
   ])
   .then((answers) => {
     const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
   })
   .catch((error) => {
     if (error.isTtyError) {
