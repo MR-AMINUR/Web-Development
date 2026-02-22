aae6344b2b156dec48bd380678b6ea16efc98606
AMINUR <aminur01mallick@gmail.com>
2026-02-22 22:44:36 +0530

    Small Exercise.

--- backend/Project/index.js
+++ backend/Project/index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    console.log(req.rawHeaders);
+    res.send("<h1>Hi</h1>");
+});
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me</h1><p>Myself Aminur</p>");
+});
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact me at</h1><p>+91123145678</p>");
 });
 
 app.listen(3000, () => {
