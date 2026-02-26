964f4439c3a33cebfe9195978c9098f0f923e5c9
AMINUR <aminur01mallick@gmail.com>
2026-02-26 22:01:43 +0530

    Practice.

--- frontend/script.js
+++ frontend/script.js
@@ -33,26 +33,31 @@
 3. Create a txt file to save the user input using the native fs node module.
 */ 
 
-import inquirer from 'inquirer';
-import qr from "qr-image";
-import fs from "fs";
+// import inquirer from 'inquirer';
+// import qr from "qr-image";
+// import fs from "fs";
 
-inquirer
-  .prompt([
-    {
-        message: "Type in your URL: ",
-        name: "URL",
-    },
-  ])
-  .then((answers) => {
-    const url = answers.URL;
-    var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+// inquirer
+//   .prompt([
+//     {
+//         message: "Type in your URL: ",
+//         name: "URL",
+//     },
+//   ])
+//   .then((answers) => {
+//     const url = answers.URL;
+//     var qr_svg = qr.image(url);
+//     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
+
+var a = 5;
+var b = 7;
+var c = a * b;
+console.log(c);
