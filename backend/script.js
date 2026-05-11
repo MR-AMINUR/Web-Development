f909223a9c8a2cc8327355862f194adf34ed9b56
MR-AMINUR <aminur01mallick@gmail.com>
2026-05-11 16:16:50 +0000

    Basics of JavaScript

--- backend/script.js
+++ backend/script.js
@@ -1,1 +1,12 @@
-console.log("Aminur");
+// console.log("Aminur");
+const accountId = 12234 
+let email = "aminur@gmail.com"
+var password = "0000"
+// console.log(accountId);
+
+/*
+    Prefer not to use var
+    because of issue in block scope and functional scope 
+*/ 
+
+console.table([accountId, email, password])
