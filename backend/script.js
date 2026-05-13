c292c92273d9715e26192fea574d643996c55d64
MR-AMINUR <aminur01mallick@gmail.com>
2026-05-13 14:18:11 +0000

    Conversion Operation

--- backend/script.js
+++ backend/script.js
@@ -9,4 +9,28 @@ var password = "0000"
     because of issue in block scope and functional scope 
 */ 
 
-console.table([accountId, email, password])
+//console.table([accountId, email, password])
+
+/* ---------------Conversion Operation --------------------*/
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 -> true 0 -> false
+// Empty String -> false
+// full String -> true
